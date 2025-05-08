0ae8253a5f80b4cb1e6103490c14da2e5e430c02
matheusgastal <mattimp12@gmail.com>
2025-05-08 13:44:25 -0300

    fixed move_membro

--- functions.h
+++ functions.h
@@ -26,5 +26,5 @@ void insere_sem_compressao(char *archive, char **arquivos, int n);
 void lista_informacoes(char *archive);
 void extrai_arquivos(char *archive, char **arquivos, int n);
 void remove_arquivos(char *archive, char **arquivos, int n);
-
+void move_membro(char *archive, char *nome_mover, char *nome_target);
 #endif
